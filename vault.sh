7a4e27b5203f0aa489d5c3cdfc311c6b88ec491a
Andrew Spratley <aps831@yahoo.co.uk>
2022-02-27 22:12:12 +0000

    feat: add download of open data

--- vault.sh
+++ vault.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 (
 cat << EOF
-nas_password: $(pass odin.local/andrew/password)
+vault_nas_password: $(pass odin.local/andrew/password | base64)
+vault_opendata_password_aps831: $(pass datafeeds.networkrail.co.uk/aps831@yahoo.co.uk/password | base64)
+vault_opendata_password_operations_reports_831: $(pass datafeeds.networkrail.co.uk/operations.reports.831@gmail.com/password | base64)
 EOF
 ) | ansible-vault encrypt - > inventory/vaulted_vars/vault.yml
