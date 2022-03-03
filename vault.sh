c09b6d1e36bac46e4ee0a0e4e835bc566c5f4bf2
Andrew Spratley <aps831@yahoo.co.uk>
2022-03-03 23:15:10 +0000

    fix: store credential as base64 for opendata

--- vault.sh
+++ vault.sh
@@ -2,7 +2,7 @@
 (
 cat << EOF
 vault_nas_password: $(pass odin.local/andrew/password | base64)
-vault_opendata_password_aps831: $(pass datafeeds.networkrail.co.uk/aps831@yahoo.co.uk/password | base64)
-vault_opendata_password_operations_reports_831: $(pass datafeeds.networkrail.co.uk/operations.reports.831@gmail.com/password | base64)
+vault_opendata_basic_auth_aps831: $(echo -n aps831@yahoo.co.uk:$(pass datafeeds.networkrail.co.uk/aps831@yahoo.co.uk/password) | base64 )
+vault_opendata_basic_auth_operations_reports_831: $(echo -n operations.reports.831@gmail.com:$(pass datafeeds.networkrail.co.uk/operations.reports.831@gmail.com/password) | base64 )
 EOF
 ) | ansible-vault encrypt - > inventory/vaulted_vars/vault.yml
