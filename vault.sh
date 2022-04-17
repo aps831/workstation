f5ca3f380f6a8e5467aa060a4560c0d2b407d8f6
Andrew Spratley <aps831@yahoo.co.uk>
2022-04-17 21:35:08 +0100

    feat: improve upgrade of gogs

--- vault.sh
+++ vault.sh
@@ -4,5 +4,6 @@ cat << EOF
 vault_nas_password: $(pass odin.local/andrew/password | base64)
 vault_opendata_password_aps831: $(pass datafeeds.networkrail.co.uk/aps831@yahoo.co.uk/password | base64 )
 vault_opendata_password_operations_reports_831: $(pass datafeeds.networkrail.co.uk/operations.reports.831@gmail.com/password | base64 )
+vault_gogs_secret: $(pass titan.local/gogs/andrew/gogs-secret-key | base64 )
 EOF
 ) | ansible-vault encrypt - > inventory/vaulted_vars/vault.yml
