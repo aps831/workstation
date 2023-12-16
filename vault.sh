70ab091afdbecb58cf01283b2f28f11ecc42f87e
Andrew Spratley <aps831@yahoo.co.uk>
2023-12-16 11:48:41 +0000

    feat: use host based user for login to odin

--- vault.sh
+++ vault.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 set -e
-vault_nas_password="$(pass odin.local/andrew/password)"
-vault_nas_password_b64="$(echo "${vault_nas_password}" | base64)"
+vault_nas_titan_password="$(pass odin.local/titan/password)"
+vault_nas_titan_password_b64="$(echo "${vault_nas_titan_password}" | base64)"
+vault_nas_thor_password="$(pass odin.local/titan/password)" # Currently no thor user set up
+vault_nas_thor_password_b64="$(echo "${vault_nas_thor_password}" | base64)"
 vault_opendata_password_aps831="$(pass datafeeds.networkrail.co.uk/aps831@yahoo.co.uk/password)"
 vault_opendata_password_aps831_b64=$(echo "${vault_opendata_password_aps831}" | base64)
 vault_opendata_password_operations_reports_831="$(pass datafeeds.networkrail.co.uk/operations.reports.831@gmail.com/password)"
 vault_opendata_password_operations_reports_831_b64=$(echo "${vault_opendata_password_operations_reports_831}" | base64)
 temp_file=$(mktemp)
 cat <<EOF >"${temp_file}"
-vault_nas_password: "${vault_nas_password_b64}"
+vault_nas_titan_password: "${vault_nas_titan_password_b64}"
+vault_nas_thor_password: "${vault_nas_thor_password_b64}"
 vault_opendata_password_aps831: "${vault_opendata_password_aps831_b64}"
 vault_opendata_password_operations_reports_831: "${vault_opendata_password_operations_reports_831_b64}"
 EOF
